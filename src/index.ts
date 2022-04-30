a6590d16a1502a485c88bad4bb61a8bc3c89d342
Vladimir Talan <talan.vladimir@gmail.com>
2022-04-30 22:54:29 +0300

    fork: add shipper and gui.

--- src/index.ts
+++ src/index.ts
@@ -26,7 +26,11 @@ class Gui {
 }
 
 class Client {
-  constructor(gui: Gui) {}
+  gui;
+
+  constructor(gui: Gui) {
+    this.gui = gui;
+  }
 
   onShip(shipment: Shipment) {}
 }
@@ -42,11 +46,59 @@ class Shipment {
     return this.state.shipmentId;
   }
 
-  ship(): string {
-    return 'ship';
+  ship(state: State) {
+    if (state.weight < 15) {
+      return this.Letter();
+    } else if (state.weight <= 160) {
+      return this.Package();
+    } else {
+      return this.Oversize();
+    }
   }
+
+  Letter() {}
+
+  Package() {}
+
+  Oversize() {}
 }
 
 class Shipper {
-  getCost() {}
+  state: State;
+  price: number;
+
+  constructor(state: State) {
+    this.state = state;
+  }
+
+  getCost(state: State) {
+    if (state.fromZipCode.length > 0) {
+      const zipStart = parseFloat(state.fromZipCode.toString().substring(0, 1));
+      if ([4, 5, 6].includes(zipStart)) {
+        this.PacificParcelShipper();
+      } else if ([7, 8, 9].includes(zipStart)) {
+        this.ChicagoSprintShipper();
+      } else {
+        this.AirEastShipper();
+      }
+    }
+  }
+
+  AirEastShipper() {
+    console.log('Air East');
+    this.price = 0.39;
+    return false;
+  }
+
+  PacificParcelShipper() {
+    console.log('Pacific Parcel');
+    this.price = 0.51;
+    return false;
+  }
+
+  ChicagoSprintShipper() {
+    console.log('Chicago Sprint');
+    this.price = 0.42;
+    return false;
+  }
 }
