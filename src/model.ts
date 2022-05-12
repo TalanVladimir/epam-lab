9898ff67672ed8c6b3f1a581c1e6363484ff851b
Vladimir Talan <talan.vladimir@gmail.com>
2022-05-12 22:25:39 +0300

    fork: add default model

--- src/model.ts
+++ src/model.ts
@@ -1,5 +1,16 @@
-import { iModel } from './types';
+import { iCurrency, iModel } from './types';
+
+const defaultArray: iCurrency[] = [
+  { from: 'Euro', to: 'USD', rate: 1.0408, fromValue: 100, toValue: 1 },
+  { from: 'Euro', to: 'JPY', rate: 133.85, fromValue: 100, toValue: 1 },
+  { from: 'Euro', to: 'BGN', rate: 1.9558, fromValue: 100, toValue: 1 },
+  { from: 'Euro', to: 'CZK', rate: 24.925, fromValue: 100, toValue: 1 },
+  { from: 'Euro', to: 'DKK', rate: 7.4413, fromValue: 100, toValue: 1 },
+];
 
 export class Model implements iModel {
-  constructor() {}
+  items: iCurrency[];
+  constructor(items = defaultArray) {
+    this.items = items;
+  }
 }
